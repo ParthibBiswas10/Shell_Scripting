4f069771e6570dfb20f34562324bc06f7655628c
ParthibBiswas10 <parthiblm10@gmail.com>
2025-08-21 01:18:42 +0530

    Searching

---
+++ search.sh
@@ -0,0 +1,18 @@
+search(){
+    key=$1
+    shift
+    arr=("$@")
+    echo ${arr[@]}
+    for((i=0;i<${#arr[@]};i++));do
+        if [ ${arr[i]} -eq ${key} ];then
+            echo "${key} present in $((i+1)) position"
+            return 
+        fi
+    done
+    echo "Not Present"
+    return 0
+
+}
+read -p "Enter numbers separated by spaces: " -a nums
+read -p "Enter key to Search:: " key
+search "$key" "${nums[@]}"
